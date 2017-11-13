ef92839fac884089ff76396ef732fb6b66f0b2ab
sxoxs <33289095+sxoxs@users.noreply.github.com>
2017-11-13 16:20:07 +0300

    Каюсь, не было времени всё привести в порядок.         Выходные вылетели.         Технически оно работает.         Позже доправлю менюшку и нормальные быстрые выборы дать и добавление задач.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,6 @@ import java.text.ParseException;
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
-//        Каюсь, не было времени всё привести в порядок.
-//        Завал на работе и выходные вылетели.
-//        Технически оно работает
-//        Позже доправлю менюшку и нормальные быстрые выборы дать и добавление задач
-
-//        Знаю, это плохой тон писать так коменты
 
         User user = new User();
         user.setName("Denis");
